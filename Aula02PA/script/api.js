c250a53db934fb06005364ad1a155b7f1ee0a882
Leo-G-Lops <giacobolopes3@gmail.com>
2026-01-31 13:11:06 -0300

    Add files via upload

--- Aula02PA/script/api.js
+++ Aula02PA/script/api.js
@@ -1,8 +1,52 @@
-const API_URL = 'http://localhost:500/contatos';
+// const STORAGE_KEY = 'contatos_api';
+
+// function obterContatos() {
+//     return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+// }
+
+// function salvarContatos(contatos) {
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(contatos));
+// }
+
+// export function getContatos() {
+//     return new Promise(resolve => {
+//         resolve(obterContatos());
+//     });
+// }
+
+// export function postContato(contato) {
+//     return new Promise(resolve => {
+//         const contatos = obterContatos();
+//         contatos.push(contato);
+//         salvarContatos(contatos);
+//         resolve();
+//     });
+// }
+
+// export function putContato(index, contato) {
+//     return new Promise(resolve => {
+//         const contatos = obterContatos();
+//         contatos[index] = contato;
+//         salvarContatos(contatos);
+//         resolve();
+//     });
+// }
+
+// export function deleteContato(index) {
+//     return new Promise(resolve => {
+//         const contatos = obterContatos();
+//         contatos.splice(index, 1);
+//         salvarContatos(contatos);
+//         resolve();
+//     });
+// }
+
+
+const API_URL = 'http://localhost:5000/contatos';
 
 export function getContatos(){
     return fetch(API_URL)
-    .then(res => res.json())
+    .then(res => res.json());
 }
 
 
@@ -20,7 +64,7 @@ export function putContato(id, contato){
     return fetch(`${API_URL}/${id}`, {
         method: 'PUT',
         headers: {
-            'Content-Type' : application/json
+            'Content-Type' : 'application/json'
         },
         body: JSON.stringify(contato)
     });
--- Aula02PA/script/script.js
+++ Aula02PA/script/script.js
@@ -1,161 +1,212 @@
-import { validarContato } from "./validations.js";
+// import {
+//     listarContatos,
+//     criarContato,
+//     atualizarContato,
+//     excluirContato
+// } from './fakeApi.js';
+import { validarContato } from './validations.js'
 
 import {
     getContatos,
     postContato,
     putContato,
     deleteContato
-} from "./api.js";
+} from './api.js';
 
-// Selecionar elementos da página
+
 const form = document.getElementById('formContato');
 const lista = document.getElementById('listaContatos');
 
 
-//let contatos =[];
-// if (localStorage.getItem('contatos')){
+let indiceEdicao = null;
+// let contatos = [];
+
+// if (localStorage.getItem('contatos')) {
 //     contatos = JSON.parse(localStorage.getItem('contatos'));
 //     mostrarContatos();
 // }
 
-let indiceEdicao = null;
+function carregarContatos() {
 
-
-function carregarContatos(){
     getContatos().then(contatos => {
-    lista.innerHTML = '';
 
-    contatos.forEach((contato) => {
-        const item = document.createElement('li');
+        lista.innerHTML = '';
 
-        item.innerHTML = `
-        <p><strong>Nome:</strong> ${contato.nome}</p>
-        <p><strong>E-mail:</strong> ${contato.email}</p>
-        <p><strong>Telefone:</strong> ${contato.telefone}</p>
-        `;
+        contatos.forEach((contato) => {
+            const item = document.createElement('li');
 
-        const botaoExcluir = item.querySelector('.btn-excluir');
-        botaoExcluir.addEventListener('click', () =>{
-            const confirmar = confirm('Deseja realmente excluir este contato?');
-            if (confirmar){
-                deleteContato(contato.id).then(() =>{
-                    alert('Contato excluído com sucesso');
-                    carregarContatos();
-                })
-                .catch(() => {
-                    alert('Erro ao excluir contato')
-                });
-            }
+            item.innerHTML = `
+                <p><strong>Nome:</strong> ${contato.nome}</p>
+                <p><strong>E-mail:</strong> ${contato.email}</p>
+                <p><strong>Telefone:</strong> ${contato.telefone}</p>
+                <button class='btn-editar'>Editar</button> 
+                <button class='btn-excluir'>Excluir</button>
+            `;
+            // const botaoExcluir = item.querySelector('.btn-excluir');
+        
+            // botaoExcluir.addEventListener('click', function() {
+            //     deleteContato(contato.id).then(carregarContatos);
+            // });
+
+            const botaoExcluir = item.querySelector('.btn-excluir');
+            botaoExcluir.addEventListener('click', () => {
+                const confirmar = confirm('Deseja realmente excluir este contato?');
+                if (confirmar) {
+                    deleteContato(contato.id).then(() => {
+                        alert('Contato excluído com sucesso');
+                        carregarContatos();
+                    })
+                    .catch(() => {
+                        alert('Erro ao excluir contato');
+                    });
+                }
+            });
+        
+            const botaoEditar = item.querySelector('.btn-editar');
+        
+            botaoEditar.addEventListener('click', function() {
+                editarContato(contato);
+            });
+        
+            lista.appendChild(item);
         });
-        const botaoEditar = item.querySelector('.btn-editar');
-        botaoEditar.addEventListener('click', function(){
-            editarContato(contato);
-        });
-        lista.appendChild(item);
-    });
-})
-.catch(()=>{
-    lista.innterHTML = '<li>Erro ao carregar contatos</li>';
-});
-} 
-
+     })
+     .catch(() => {
+        lista.innerHTML = '<li>Erro ao carregar contatos</li>';
+     });
+}
 let contatoEmEdicao = null;
 
-function editarContato(contato){
-    nome.value = contato.nome;
-    email.value = contato.email;
-    telefone.value = contato.telefone;
-    contatoEmEdicao = contato;
+function editarContato(contato) {
+    // const contato = contatos[index];
 
-    const botao = form.querySelector('button');
-    botao.textContent = 'Atualizar contato';
-    form.scrollIntoView({behavior: 'smooth'});
-}
+        // document.getElementById('nome').value = contato.nome;
+        // document.getElementById('email').value = contato.email;
+        // document.getElementById('telefone').value = contato.telefone;
 
+        nome.value = contato.nome;
+        email.value = contato.email;
+        telefone.value = contato.telefone;
+        
+        contatoEmEdicao = contato;
 
-form.addEventListener('submit', function(event){
+        const botao = form.querySelector('button');
+        botao.textContent = 'Atualizar contato';
+
+        form.scrollIntoView({ behavior: 'smooth' });
+    }
+
+// function removerContato(index) {
+//     // contatos.splice(index, 1);
+
+//     // localStorage.setItem('contatos', JSON.stringify(contatos));
+//     deleteContato(index).then(() => {
+
+//         mostrarContatos();
+//     });
+// }
+
+// let indiceEdicao = null;
+
+form.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    // botao.textContent = 'Salvar contato';
+
     const contato = {
-        nome: nome.value.trim(), 
-        email: email.value.trim(), 
+        nome: nome.value.trim(),
+        email: email.value.trim(),
         telefone: telefone.value.trim()
+       
     };
-    if (!validarContato(contato)){
+
+    if (!validarContato(contato)) {
         return;
     }
-    if(!contato.nome || !contato.email || contato.telefone){
-        alert('Preencha todos os campos');
-        return;
+
+
+    if (!contato.nome || !contato.email || !contato.telefone) {
+       alert('Preencha todos os campos');
+       return;
     }
-    if(contatoEmEdicao){
-        putContato(contatoEmEdicao.id, contato).then(()=>{
-            alert('Contato atualizado com sucesso');
-            contatoEmEdicao = null;
-            form.reset;
-            form.querySelector('button').textContent = 'Salvar Contato';
-            carregarContatos();
-        })
-        .catch(()=>{
-            mostrarMensagem('Erro ao atualizar contato')
-        });
-    } else {
-        postContato(contato).then(()=>{
-            alert('Contato cadastrado com sucesso')
-            form.reset();
-            carregarContatos();
-        })
-        .catch(()=>{
-            alert('Erro ao cadastrar contato');
-        });
-    }
-});
-
-carregarContatos();
-
-
-
-// Evento de envio do formulário
-
-// form.addEventListener('submit',function(event){
-//     event.preventDefault(); //impede de recarregar a página
 
     
-//     //pegar valores digitados
-//     const nome = document.getElementById('nome').value;
-//     const email = document.getElementById('email').value;
-//     const telefone = document.getElementById('telefone').value;
-    
-    //validação
-    
-    // if(nome === "" || email === "" || telefone === ""){
-    //     alert('Preencha todos os campos!')
+    // const nome = document.getElementById('nome').value;
+    // const email = document.getElementById('email').value;
+    // const telefone = document.getElementById('telefone').value;
+
+    // if (nome === '' || email === '' || telefone === '') {
+    //     alert('Preencha todos os campos');
     //     return;
     // }
 
+    // const contato = { nome, email, telefone };
+
+    // if (indiceEdicao === null) {
+    //     // contatos.push({ nome, email, telefone });
+    //     postContato(contato).then(() => {
+    //         mostrarContatos();
+    //     });
+    // } else {
+    //     // contatos[indiceEdicao] = { nome, email, telefone };
+    //     putContato(indiceEdicao, contato).then(() => {
+
+    //         indiceEdicao = null;
+    //         mostrarContatos();
+    //     });
+    // }
+
+    if (contatoEmEdicao) {
+        putContato(contatoEmEdicao.id, contato)
+            .then(() => {
+                alert('Contato atualizado com sucesso');
+                contatoEmEdicao = null;
+                form.reset();
+                form.querySelector('button').textContent = 'Salvar contato';
+                carregarContatos();
+            })
+            
+            .catch(() => {
+                mostrarMensagem('Erro ao atualizar contato');
+            });
+           
+    } else {
+        postContato(contato)
+            .then(() => {
+                alert('Contato cadastrado com sucesso');
+                form.reset();
+                carregarContatos();
+            })
+            .catch(() => {
+                alert('Erro ao cadastrar contato');
+            });
+            
+    }
+});
+
+
     // const novoContato = {
-    //     nome:nome,
-    //     email:email,
-    //     telefone: tel
+    //     nome: nome,
+    //     email: email,
+    //     telefone: telefone
     // };
 
     // contatos.push(novoContato);
 
-   // localStorage.setItem('contatos', JSON.stringify(contatos));
+    // localStorage.setItem('contatos', JSON.stringify(contatos));
 
-    //mostrarContatos();
-    // //criando item da lista
+    // mostrarContatos();
     // const item = document.createElement('li');
-    // item.textContent = `Nome: ${nome} | E-mail:${email} | Telefone:${tel}`;
-    
-    // //adicionar na lista
+    // // item.textContent = `Nome: ${nome} | E-mail: ${email} | Telefone: ${telefone}`;
+    // item.innerHTML = `
+    //     <strong>Nome:</strong> ${nome}<br>
+    //     <strong>E-mail:</strong> ${email}<br>
+    //     <strong>Telefone:</strong> ${telefone}<br>
+    // `
+
     // lista.appendChild(item);
 
-    //limpar os campos
-    //form.reset();
-    
-//});
+   // form.reset();
 
 
-
-
+carregarContatos();
